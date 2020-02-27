bbe72e1152d75fa6196df09ee2b5a8fb38d268d0
Tim <tstave217@gmail.com>
2020-02-26 22:46:25 -0600

    Attempting to get heroku build to work

--- webpack.config.js
+++ webpack.config.js
@@ -6,12 +6,14 @@ module.exports = () => {
 
   // call dotenv and it will return an Object with a parsed key 
   const env = dotenv.config().parsed;
-  
+  let envKeys = [];
   // reduce it to a nice object, the same as before
-  const envKeys = Object.keys(env).reduce((prev, next) => {
-    prev[`process.env.${next}`] = JSON.stringify(env[next]);
-    return prev;
-  }, {});
+  if (typeof Object.keys(env) !== 'undefined' && Object.keys(env).length > 0) {
+    envKeys = Object.keys(env).reduce((prev, next) => {
+      prev[`process.env.${next}`] = JSON.stringify(env[next]);
+      return prev;
+    }, {});
+  }
 
   return {
     entry: './public/ts/index.ts',
